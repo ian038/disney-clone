04390cdc4e5de1633a5800e7fc133bbfd0d1765d
ian038 <ianphua3@gmail.com>
2022-01-25 20:57:14 -0500

    Cleanup app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Login from './components/Login';
+import Home from './components/Home';
+import Detail from './components/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello world
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
