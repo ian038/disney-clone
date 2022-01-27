dd5d4d33618048187b9011fa818fef3a3476d63a
ian038 <ianphua3@gmail.com>
2022-01-26 21:30:12 -0500

    Completed recommends component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,19 +2,58 @@ import React, { useEffect } from 'react';
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import db from "../firebase";
-// import { setMovies } from "../features/movie/movieSlice";
+import { setMovies } from "../redux/reducers/movie";
 import { selectUserName } from "../redux/reducers/user";
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
+import Recommends from './Recommends';
 
 export default function Home() {
     const dispatch = useDispatch();
     const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = []
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                switch (doc.data().type) {
+                    case "recommend":
+                        recommends = [...recommends, { id: doc.id, ...doc.data() }];
+                        break;
+
+                    case "new":
+                        newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+                        break;
+
+                    case "original":
+                        originals = [...originals, { id: doc.id, ...doc.data() }];
+                        break;
+
+                    case "trending":
+                        trending = [...trending, { id: doc.id, ...doc.data() }];
+                        break;
+                }
+            });
+
+            dispatch(
+                setMovies({
+                    recommend: recommends,
+                    newDisney: newDisneys,
+                    original: originals,
+                    trending: trending
+                })
+            );
+        });
+    }, [userName])
 
     return (
         <Container>
             <ImgSlider />
             <Viewers />
+            <Recommends />
         </Container>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './reducers/user'
+import movieReducer from "./reducers/movie";
 
 export default configureStore({
     reducer: {
-        user: userReducer
-        // movie: movieReducer
+        user: userReducer,
+        movie: movieReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
